8bbf97e12286318f2e408fde238bad1f55fa3547
Mi <105437786+Milenaalessi@users.noreply.github.com>
2024-09-06 23:31:04 -0300

    dsdssdsd

---
+++ script.js
@@ -0,0 +1,84 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Qual é o seu tipo de esporte preferido?",
+        alternativas: [
+            {
+                texto: "Esportes físicos.",
+                afirmacao: "É preciso ter um bom jogo de corpo para se impor no basquete, assim como uma perna calibrada para acertar um bom chute no futebol."
+            },
+            {
+                texto: "Esportes mentais.",
+                afirmacao: "Não basta apenas saber mover as pecinhas: é preciso ter estratégia no xadrez, a mais famosa das modalidades de esporte principalmente mentais.."
+            }
+
+        ]
+    },
+    {
+        enunciado: "Esporte individual ou em equipe??",
+        alternativas: [
+            {
+                texto: "Individual.",
+                afirmacao: "São aqueles praticados por apenas uma pessoa, ou seja, um atleta compete com adversário individualmente."
+            },
+            {
+                texto: "Em equipe.",
+                afirmacao: "Praticados por duas ou mais pessoas em oposição à equipe adversária."
+            }
+
+        ]
+    },
+    {
+        enunciado: "Você gosta de esportes que usam",
+        alternativas: [
+            {
+                texto: "Pés.",
+                afirmacao: "hyfgh"
+            },
+            {
+                texto: "Mãos.",
+                afirmacao: "o."
+            }
+        ]
+    }
+];
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    textoResultado.textContent
+    mostraAlternativas();
+
+}
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativa = document.createElement("button");
+        botaoAlternativa.textContent = alternativa.texto;
+        botaoAlternativa.addEventListener("click", () =>respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativa);
+    }
+}
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacao = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacao + " ";
+    atual++
+    mostraPergunta();
+}
+function mostraResultado() {
+    caixaPerguntas.textContent = "Resumindo...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
+mostraPergunta();
